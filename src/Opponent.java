b5711504fe776f2d6e86d1fd093c438ebfd2dcbe
oscar <oscgueviv1987@gmail.com>
2026-06-06 23:56:40 -0700

    Added opponent and added more tests, will now start working on Game class and its test to build all the foundations

---
+++ src/Opponent.java
@@ -0,0 +1,77 @@
+public class Opponent extends Character {
+
+    public Opponent(String name, int balance) {
+        super(name, balance);
+    }
+
+    //used in the situation where the player bets first
+    public int counterBet(Player player, int playerBet) {
+      int oppBet = 0;
+       //gets the aura of the player
+        float playerAura = player.getAura();
+        //gets the aura of the NPC
+        float opponentAura = getAura();
+
+        if (playerAura < opponentAura && getBalance() < player.getBalance()){
+            System.out.println(getName() + ": You think I'm scared of you?");
+            System.out.println(getName() + ": ... I'm putting my all into this one, winner takes all");
+            oppBet = getBalance();
+        }
+        else if (playerAura > opponentAura) {
+            System.out.println(getName() + ": ... lets make this game fun");
+            if (getBalance() < playerBet) {
+                oppBet = playerBet/2;
+            }
+            else {
+                oppBet = playerBet;
+            }
+        }
+        else if (playerAura == opponentAura) {
+            int choice = (int)(Math.random() * 2) + 1;
+            if (choice == 1 && getBalance() > playerBet) {
+                System.out.println(getName() + ": I'll play it smart...");
+                oppBet = playerBet/3;
+                }
+            else if (choice == 1 && getBalance() <= playerBet) {
+                int random = (int) (Math.random() * 5) + 1;
+                System.out.println(getName() + ": I'll leave it up to fate");
+                oppBet = playerBet/random;
+            }
+            else if (choice == 2 && getBalance() > playerBet){
+                System.out.println(getName() + ": You're pathetic.... you haven't realized the trick yet.,");
+                oppBet = 1;
+            }
+            else if (choice == 2 && getBalance() <= playerBet) {
+                System.out.println(getName() + ": lets see if I can survive....");
+                oppBet = playerBet;
+            }
+        }
+        return oppBet;
+    }
+
+
+
+    //used in the situation where the opponent bets first
+    public int placeBet(){
+        int oppBet = 0;
+        System.out.println(getName() + ": ... I wonder what I'll bet against you...");
+        int choice = (int) (Math.random() * 10) + 1;
+        if (choice == 1){
+            System.out.println(getName() + ": Lets see if fortune favors the bold...");
+            oppBet = getBalance();
+        }
+        else if (choice == 2 || choice == 10) {
+            System.out.println(getName() + ": Lets play it fun");
+            oppBet = getBalance()/2;
+        }
+        else if (choice == 3 || choice == 9 || choice == 6) {
+            System.out.print(getName() + ": okay...");
+            oppBet = getBalance()/3;
+        }
+        else {
+            System.out.println(getName() + ": ......");
+            oppBet = getBalance()/4;
+        }
+        return oppBet;
+    }
+}
---
+++ src/OpponentTest.java
@@ -0,0 +1,12 @@
+public class OpponentTest {
+    public static void main(String[] args){
+        Opponent opp = new Opponent("Jeffery", 300);
+        Player play = new Player("Bruh", 500);
+      //same aura no aura added
+      //for more aura comment out .play.addAura
+        opp.addAura(20);
+       // play.addAura(30);
+       int bet = play.chooseBet();
+       System.out.println(opp.counterBet(play, bet));
+    }
+}
